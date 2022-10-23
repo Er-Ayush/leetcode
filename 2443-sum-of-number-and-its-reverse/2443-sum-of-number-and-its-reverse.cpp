f01abb869d5964d8f130551510e8a7e4788f051b
Coder-Ayush <89703959+Er-Ayush@users.noreply.github.com>
2022-10-23 11:31:33 +0530

    Time: 55 ms (90.18%), Space: 5.7 MB (99.16%) - LeetHub

---
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool sumOfNumberAndReverse(int num) {
+    auto rev = [](int n) {
+        int rn = 0;
+        for (; n; n /= 10)
+            rn = rn * 10 + n % 10;
+        return rn;
+    };  
+    for (int n1 = num / 2; n1 <= num; ++n1)
+        if (n1 + rev(n1) == num)
+            return true;
+    return false;
+}
+
+};
