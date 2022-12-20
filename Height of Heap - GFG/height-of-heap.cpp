d03da8fd92870e883c2a5e37cdef1237d84c71a4
Coder-Ayush <89703959+Er-Ayush@users.noreply.github.com>
2022-12-20 18:40:22 +0530

    Added solution - LeetHub

---
+++ Height of Heap - GFG/height-of-heap.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int heapHeight(int N, int arr[]){
+        // code here
+        int ct=0;
+        int j=1;
+       for(int i=1;i<N;i+=j){
+           
+           ct++;
+           j*=2;
+           
+       }
+       return ct;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() { 
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin >> N;
+        int arr[N];
+        for(int i = 0; i < N; i++)
+            cin >> arr[i];
+        
+        Solution ob;
+        cout << ob.heapHeight(N, arr) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
