499836e5a934182ec678d5461fbd7961c0d310d8
Coder-Ayush <89703959+Er-Ayush@users.noreply.github.com>
2022-12-28 19:51:58 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -5,42 +5,32 @@ using namespace std;
 // } Driver Code Ends
 
 class Solution {
-  public:
-    // Function to find the number of islands.
+public:
     int numIslands(vector<vector<char>>& grid) {
-        // Code here
-        int ans=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
-                if(grid[i][j]=='1'){
-                    dfs(i,j,grid);
-                    ans++;
+        int res=0;
+        for(int i=0;i<grid.size();i++)
+            for(int j=0;j<grid[i].size();j++)
+                if(grid[i][j]=='1')// if 1, call the dfs and make res++ to add this island 
+                {
+                    dfs(grid,i,j);
+                    res++;
                 }
-            }
-        }
-        return ans;
+        return res;
     }
-    
-    void dfs(int i,int j,vector<vector<char>>& v){
+    void dfs(vector<vector<char>>& v,int i,int j)
+    {
+        if(i<0 || j<0 || i>=v.size() || j>=v[0].size() || v[i][j]=='0')
+            return;
         
-        if(i<0 || j<0 || i>=v.size() || j>=v[0].size() || v[i][j]=='0') return;
-        v[i][j]='0';
-        // grid[i][j+1];
-        // grid[i][j-1];
-        // grid[i-1][j];
-        // grid[i+1][j];
-        // grid[i+1][j+1];
-        // grid[i-1][j+1];
-        // grid[i+1][j-1];
-        // grid[i-1][j-1];
-        dfs(i-1,j,v);
-        dfs(i+1,j,v);
-        dfs(i,j-1,v);
-        dfs(i,j+1,v);
-        dfs(i+1,j+1,v);
-        dfs(i+1,j-1,v);
-        dfs(i-1,j+1,v);
-        dfs(i-1,j-1,v);
+        v[i][j]='0';// reset to 0 as this is already included
+        dfs(v,i-1,j);
+        dfs(v,i+1,j);
+        dfs(v,i,j-1);
+        dfs(v,i,j+1);
+        dfs(v,i-1,j-1);
+        dfs(v,i+1,j+1);
+        dfs(v,i+1,j-1);
+        dfs(v,i-1,j+1);
     }
 };
 
