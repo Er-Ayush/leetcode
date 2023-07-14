26fac2eb4aa36bf772924c618155791b724528ca
Coder-Ayush <89703959+Er-Ayush@users.noreply.github.com>
2023-07-14 17:11:51 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -3,36 +3,39 @@
 using namespace std;
 
 // } Driver Code Ends
-
 class Solution {
-  private:
-    bool detect(vector<int> adj[] , int vis[] , int i) {
-        vis[i] = 1;
-        queue<pair<int , int>> q;
-        q.push({i , -1});
+  public:
+    // Function to detect cycle in an undirected graph.
+    
+    bool check(int src,vector<int> adj[],int vis[]){
+        vis[src]=1;
+        queue<pair<int,int>> q;
+        q.push({src,-1});
         
-        while(!q.empty()) {
-            int node = q.front().first;
-            int parent = q.front().second;
+        while(!q.empty()){
+            int node=q.front().first;
+            int parent=q.front().second;
             q.pop();
-            
-            for(auto adjNodes : adj[node]) {
-                if(!vis[adjNodes]) {
-                    vis[adjNodes] = 1;
-                    q.push({adjNodes , node});
+            for(auto i:adj[node]){
+                if(!vis[i]){
+                    vis[i]=1;
+                    q.push({i,node});
                 }
-                else if(parent != adjNodes) return true;
+                else{
+                    if(i!=parent) return true;
+                }
             }
         }
         return false;
-    } 
-  public:
+    }
+    
     bool isCycle(int V, vector<int> adj[]) {
-        int vis[V] = {0};
-        for(int i = 0 ; i < V ; i++) {
-            if(vis[i] == 0) {
-                if(detect(adj , vis , i)) return true;
-            }
+        // Code here
+        int vis[V]={0};
+        for(int i=0;i<V;i++){
+          if(!vis[i]){
+              if(check(i,adj,vis)) return true;
+          }   
         }
         return false;
     }
